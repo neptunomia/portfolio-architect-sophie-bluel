5bab0f0953367d7346b7ac957026eeff252ab957
Noémie Houdou <houdou.noemie@gmail.com>
2022-11-30 18:48:40 +0100

    feat: add filters and style them

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,4 +1,4 @@
-// query with fetch
+// query to retrieve all works
 fetch('http://localhost:5678/api/works')
     .then((response) => {
         if (!response.ok) {
@@ -10,16 +10,19 @@ fetch('http://localhost:5678/api/works')
         console.log(data);
         showProjects(data);
     })
+
     .catch((error) => {
         console.error('There has been a problem with your fetch operation:', error);
     })
 
+// function to create and show projects
 function showProjects(projects) {
     //console.log(projects)
     for (let project of projects) {
 
         const gallery = document.querySelector('div.gallery');
         const newFigure = document.createElement('figure');
+        newFigure.setAttribute('category-id', project.categoryId)
 
         const newImage = document.createElement('img');
         newImage.setAttribute('crossorigin', 'anonymous')
@@ -33,3 +36,32 @@ function showProjects(projects) {
         gallery.appendChild(newFigure);
     }
 }
+
+fetch('http://localhost:5678/api/categories')
+    .then((response) => {
+        if (!response.ok) {
+            throw new Error('Network response was not OK');
+        }
+        return response.json();
+    })
+    .then((categories) => {
+        console.log(categories);
+        createFilters(categories);
+    })
+
+    .catch((error) => {
+        console.error('There has been a problem with your fetch operation:', error);
+    })
+
+// function to create filters
+
+function createFilters(filtersContent) {
+    const filters = document.querySelector('ul.filters');
+
+    for (let filter of filtersContent) {
+        const newLi = document.createElement('li');
+        newLi.innerText = filter.name;
+        newLi.classList.add('filter');
+        filters.appendChild(newLi);
+    }
+};
